784b6e2b9dd8df40b9c9a88c7da54d2066151e15
ruslana-semkiv18 <156903697+ruslana-semkiv18@users.noreply.github.com>
2024-07-07 19:02:48 +0000

    Deploying to gh-pages from @ ruslana-semkiv18/goit-js-hw-09@7f96636d47fbedf13c8dd7c1b3eb0c39683db6a1 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";const e=document.querySelector(".feedback-form");e.addEventListener("input",handlerInput);
+//# sourceMappingURL=commonHelpers2.js.map
